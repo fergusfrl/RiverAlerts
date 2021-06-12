164f1b80ee3f3fe3623b1a8db9ddd22be2ce2de9
Fergus Farrell <fergus.farrell@comrad.co.nz>
2021-06-12 14:09:01 +1200

    firebase admin env vars are noe next public

--- firebaseAuth.ts
+++ firebaseAuth.ts
@@ -1,16 +1,16 @@
 import admin from 'firebase-admin';
 
 const FIREBASE_ADMIN_PARAMS = {
-  type: process.env.FIREBASE_ADMIN_TYPE,
-  projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-  privateKeyId: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
-  clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-  clientId: process.env.FIREBASE_ADMIN_CLIENT_ID,
-  authUri: process.env.FIREBASE_ADMIN_AUTH_URI,
-  tokenUri: process.env.FIREBASE_ADMIN_TOKEN_URI,
-  authProviderX509CertUrl: process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
-  clientC509CertUrl: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+  type: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_TYPE,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID,
+  privateKeyId: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY_ID,
+  privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY,
+  clientEmail: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
+  clientId: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_ID,
+  authUri: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_AUTH_URI,
+  tokenUri: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_TOKEN_URI,
+  authProviderX509CertUrl: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+  clientC509CertUrl: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
 };
 
 export const verifyIdToken = (token: string): Promise<admin.auth.DecodedIdToken> => {
