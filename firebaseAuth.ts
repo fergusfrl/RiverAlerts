b20506af8e8bdb8b986672d2d363277abd31ccca
Fergus Farrell <fergus.farrell@comrad.co.nz>
2021-06-12 13:48:29 +1200

    moved firebase-admin config to env var

--- firebaseAuth.ts
+++ firebaseAuth.ts
@@ -1,17 +1,16 @@
 import admin from 'firebase-admin';
-import serviceAccount from './secrets.json';
 
 const FIREBASE_ADMIN_PARAMS = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientC509CertUrl: serviceAccount.client_x509_cert_url,
+  type: process.env.FIREBASE_ADMIN_TYPE,
+  projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+  privateKeyId: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+  clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  clientId: process.env.FIREBASE_ADMIN_CLIENT_ID,
+  authUri: process.env.FIREBASE_ADMIN_AUTH_URI,
+  tokenUri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+  authProviderX509CertUrl: process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+  clientC509CertUrl: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
 };
 
 export const verifyIdToken = (token: string): Promise<admin.auth.DecodedIdToken> => {
